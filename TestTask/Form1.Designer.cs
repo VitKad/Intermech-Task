b80eb30a547dcd8ef927885f36fbac07a9cdb732
VitKad <74490554+VitKad@users.noreply.github.com>
2022-08-12 16:20:22 +0300

    added first version

---
+++ TestTask/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Поиск по номеру объекта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Поиск по наименованию объекта";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Поиск по описанию";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(237, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 43);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Выгрузить в Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(272, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(792, 559);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(237, 48);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Определить состав выбранного объекта";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 26);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "*щелкните на номер нужного объекта в \r\nтаблице, а затем нажите на кнопку";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(237, 41);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Сброс";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 583);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Intermech";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TestTask/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace TestTask
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection intermechConnection = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Request()
+        {
+            //Загрузка данных по написаному SQL запросу
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(
+                @"SELECT 
+                objects.F_OBJECT_ID as 'Номер объекта',
+                types.F_OBJECT_TYPE as 'Идентификатор типа объекта',
+                types.F_OBJ_NAME as 'Наименование объекта',
+                views.CAPTION as 'Описание'
+                FROM INTERMECH_BASE.dbo.IMS_OBJECTS as objects
+                INNER JOIN INTERMECH_BASE.dbo.IMS_OBJECT_TYPES as types
+                ON objects.F_OBJECT_TYPE = types.F_OBJECT_TYPE
+                INNER JOIN INTERMECH_BASE.dbo.IMS_OBJECTS_VIEW as views
+                ON objects.F_OBJECT_ID = views.F_OBJECT_ID", intermechConnection);
+
+            DataSet dataSet = new DataSet();
+
+            dataAdapter.Fill(dataSet);
+
+            //загрузка данных в GridView
+            dataGridView1.DataSource = dataSet.Tables[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //подключение к базе данных
+            intermechConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["intermech"].ConnectionString);
+
+            intermechConnection.Open();
+
+            //выполение запроса
+            Request();
+
+            //заполнение comboBox значениями из таблицы типа объектов
+            comboBox1.Items.Add("Все объекты");
+
+            SqlCommand cmd = new SqlCommand(@"SELECT 
+                types.F_OBJ_NAME as 'Наименование объекта'
+                FROM INTERMECH_BASE.dbo.IMS_OBJECT_TYPES as types", intermechConnection);
+
+            SqlDataReader DR = cmd.ExecuteReader();
+
+            while (DR.Read())
+            {
+                comboBox1.Items.Add(DR[0]);
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //выгрузка элементов таблицы в Excel файл
+            Excel.Application exApp = new Excel.Application();
+            exApp.Workbooks.Add();
+            Excel.Worksheet wsh = (Excel.Worksheet)exApp.ActiveSheet;
+
+            int i, j;
+            for (i = 0; i <= dataGridView1.RowCount - 2; i++)
+            {
+                for (j = 0; j <= dataGridView1.ColumnCount - 1; j++)
+                {
+                    wsh.Cells[1, j + 1] = dataGridView1.Columns[j].HeaderText.ToString();
+                    wsh.Cells[i+2, j+1] = dataGridView1[j, i].Value.ToString();
+                }
+            }
+
+            wsh.Columns.EntireColumn.AutoFit();
+            exApp.Visible = true;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Request();
+            //поиск в таблице значений удовлетворяющих textBox
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter =
+                 String.Format("CONVERT([Номер объекта], 'System.String') LIKE '{0}%'", textBox1.Text);
+
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Request();
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter =
+              String.Format("CONVERT([Наименование объекта], 'System.String') LIKE '{0}'", comboBox1.SelectedItem);
+            if (comboBox1.SelectedItem == "Все объекты")
+            {
+                (dataGridView1.DataSource as DataTable).DefaultView.RowFilter =
+             String.Format("CONVERT([Наименование объекта], 'System.String') LIKE '%'");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Request();
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter =
+              String.Format("CONVERT([Описание], 'System.String') LIKE '%{0}%'", textBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //если значение не выбрано, то не ывполнять запрос
+            if (dataGridView1.CurrentCell == null)
+            {
+                return;
+            }
+
+            //запрос на получение информации о дочерних объектах
+            SqlDataAdapter structureAdapter = new SqlDataAdapter(
+               String.Format(
+               @"SELECT 
+                objects.F_OBJECT_ID as 'Номер объекта', 
+                types.F_OBJECT_TYPE as 'Идентификатор типа объекта', 
+                types.F_OBJ_NAME as 'Наименование объекта',
+                views.CAPTION as 'Описание'
+                FROM INTERMECH_BASE.dbo.IMS_OBJECTS as objects
+                INNER JOIN INTERMECH_BASE.dbo.IMS_OBJECT_TYPES as types
+                ON objects.F_OBJECT_TYPE = types.F_OBJECT_TYPE
+                INNER JOIN INTERMECH_BASE.dbo.IMS_OBJECTS_VIEW as views
+                ON objects.F_OBJECT_ID = views.F_OBJECT_ID
+                WHERE objects.F_ID IN (
+                SELECT relations.F_PART_ID as 'Потомок'
+                FROM INTERMECH_BASE.dbo.IMS_RELATIONS as relations
+                WHERE F_PROJ_ID = {0}
+                );
+                ", dataGridView1.CurrentCell.Value.ToString()
+            ), intermechConnection);
+
+            DataSet structureSet = new DataSet();
+
+            structureAdapter.Fill(structureSet);
+
+            dataGridView1.DataSource = structureSet.Tables[0];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Сброс
+            Request();
+        }
+    }
+}
